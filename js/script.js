5a805be75cedfb0b3764acafc5bdf6c4e90be886
Benstrocity <byoungblood.c@gmail.com>
2018-12-12 06:19:59 -0600

    fixed theme change in JS, working on responsiveness

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Declaring variables */
 const body = document.querySelector('body');
 const mainWrapper = document.querySelector('#mainWrapper');
 const navMenu = document.querySelector('#mainNav');
@@ -7,31 +8,36 @@ const initColorChange = `<button id='initColorChange'>Change Theme</button>`;
 colorChanger.innerHTML = initColorChange;
 const themeButton = document.querySelector('#initColorChange');
 
-const changeTheme = () => {
-    let randIndex = Math.floor(Math.random() * 2);
-    const themeArr = [
-            {
-                bodyBG: body.style.backgroundColor = 'black',
-                fontColor: mainWrapper.style.color = 'white',
-                navBG: mainNav.style.backgroundColor = 'white',
-                navFont: mainNav.style.color = 'black',
-                imageBG: profImg.style.backgroundColor = 'white',
-                buttonColor: themeButton.style.backgroundColor = 'white',
-                buttonFont: themeButton.style.color = 'black'
-            },   
-            {    
-                bodyBG: body.style.backgroundColor = 'lightgray',
-                fontColor: mainWrapper.style.color = 'brown',
-                navBG: mainNav.style.backgroundColor = 'lightgray',
-                navFont: mainNav.style.color = 'brown',
-                imageBG: profImg.style.backgroundColor = 'lightgray',
-                buttonColor: themeButton.style.backgroundColor = 'lightgray',
-                buttonFont: themeButton.style.color = 'brown'
-            }
-        ];
-    return themeArr[0];
+/* This function sets a random website theme */
+const newTheme = () => {
+    let rand = Math.floor(Math.random() * 2); //Randomize number
+    let bodyBG;
+    let fontColor;
+    let navBG;
+    let navFont;
+    let imageBG;
+    let buttonColor;
+    let buttonFont;
+    if (rand === 0) { //Theme 1
+        bodyBG = body.style.backgroundColor = 'black';
+        fontColor = mainWrapper.style.color = 'white';
+        navBG = mainNav.style.backgroundColor = 'white';
+        navFont = mainNav.style.color = 'black';
+        imageBG = profImg.style.backgroundColor = 'white';
+        buttonColor = themeButton.style.backgroundColor = 'white';
+        buttonFont = themeButton.style.color = 'black';
+    } else { //Theme 2
+        bodyBG = body.style.backgroundColor = 'lightgray';
+        fontColor = mainWrapper.style.color = 'brown';
+        navBG = mainNav.style.backgroundColor = 'white';
+        navFont = mainNav.style.color = 'brown';
+        imageBG = profImg.style.backgroundColor = 'lightgray';
+        buttonColor = themeButton.style.backgroundColor = 'lightgray';
+        buttonFont = themeButton.style.color = 'brown';
+    }
 }
 
+/* This event handler returns a random theme from the newTheme function */
 themeButton.addEventListener('click', () => {
-    changeTheme();
-})
+    newTheme();
+});
