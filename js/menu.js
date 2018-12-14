6e99221b2928c3e56c00f4cd9b9b5cd9300416fa
Ben Youngblood <byoungblood@CRHS.local>
2018-12-14 16:24:19 -0600

    finished toggle menu functionality

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,22 @@
-const toggleMenu = document.querySelector('#mainNav span');
-const navUL = document.querySelector('#mainNav ul');
+const siteLogo = document.querySelector('.homeLi');
+const toggleMenu = document.querySelector('#toggle');
+const toggleItems = document.querySelector('#toggleItems');
+toggleMenu.className = 'closed';
 
 toggleMenu.addEventListener('click', () => {
-    navUL.style.display = 'flex';
-    navUL.style.animation = 'slideFromRight 1s';
-    toggleMenu.style.display = 'none';
+    if (toggleMenu.className.includes('closed')) {
+        toggleItems.style.display = 'flex';
+        toggleItems.style.animation = 'slideFromRight 2s';
+        siteLogo.style.animation = 'fadeIn 2s';
+        toggleMenu.style.animation = 'slideFromRight 2s';
+        toggleMenu.className.replace(/\bclosed\b/, '');
+        toggleMenu.className = 'open';
+    } else {
+        toggleItems.style.display = 'none';
+        toggleItems.style.animation = 'slideFromLeft 2s';
+        siteLogo.style.animation = 'fadeIn2 2s';
+        toggleMenu.style.animation = 'slideFromLeft 2s';
+        toggleMenu.className.replace(/\bopen\b/, '');
+        toggleMenu.className = 'closed';
+    }
 });
