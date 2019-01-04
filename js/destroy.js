e2d25eba421b7019f01cfa6fb1f5e2694464a0b0
Benstrocity <byoungblood.c@gmail.com>
2019-01-04 07:12:24 -0600

    updates to comments, code formatting, content additions and edits, final styling changes and fixes, updates to destroyed background

--- js/destroy.js
+++ js/destroy.js
@@ -4,75 +4,93 @@
     Created by: Ben Youngblood on 12/12/2018
 */
 
-/* Declaring variables */
-const body = document.querySelector('body');
-const mainWrapper = document.querySelector('#mainWrapper');
-const navMenu = document.querySelector('#mainNav');
-const profImg = document.querySelector('#profileImage');
-const footer = document.querySelector('#mainFooter');
-const destruct = document.querySelector('#destruct');
-const initDestruct = `<button id='initDestruct'>Destroy Website</button>`; //Creates the destroy button
+/**** 
+  --- Declaring variables ---
+****/
+
+const body = document.querySelector("body");
+const mainWrapper = document.querySelector("#mainWrapper");
+const navMenu = document.querySelector("#mainNav");
+const profImg = document.querySelector("#profileImage");
+const footer = document.querySelector("#mainFooter");
+const destruct = document.querySelector("#destruct");
+//Creates the destroy button
+const initDestruct = `<button id='initDestruct'>Destroy Website</button>`;
 destruct.innerHTML = initDestruct;
-const destructButton = document.querySelector('#initDestruct');
-
-const repair = document.querySelector('#repair');
-const initRepair = `<button id='initRepair'>Repair Website</button>`; //Creates the repair button
+const destructButton = document.querySelector("#initDestruct");
+//Creates the repair button
+const repair = document.querySelector("#repair");
+const initRepair = `<button id='initRepair'>Repair Website</button>`;
 repair.innerHTML = initRepair;
-const repairButton = document.querySelector('#initRepair');
-repairButton.style.display = 'none';
+const repairButton = document.querySelector("#initRepair");
+repairButton.style.display = "none";
 
+/****
+  --- Build functions ---
+****/
 
-/* This function will destroy the website */
+// This function will destroy the website
 const newDestroy = () => {
-    destructButton.style.display = 'none';
-    mainWrapper.style.animation = 'fadeOut 2.5s';
+  destructButton.style.display = "none";
+  mainWrapper.style.animation = "fadeOut 2.5s";
 
-    window.setTimeout(() => {
-        if (html.clientWidth < 900) {
-            repairButton.style.display = 'block';
-            repairButton.style.animation = 'fadeIn 2.5s';
-            body.style.backgroundImage = "url('./img/background_dark.png')";
-            body.style.backgroundColor = '#551d1d';
-            body.style.backgroundPosition = 'top right';
-            mainWrapper.style.display = 'none';
-        }
-        else if (html.clientWidth < 1300) {
-            repairButton.style.display = 'block';
-            repairButton.style.animation = 'fadeIn 2.5s';
-            body.style.backgroundImage = "url('./img/background_dark.png')";
-            body.style.backgroundColor = '#551d1d';
-            body.style.backgroundPosition = 'top right';
-            body.style.height = '1100px';
-            mainWrapper.style.display = 'none';
-        } else {
-            repairButton.style.display = 'block';
-            repairButton.style.animation = 'fadeIn 2.5s';
-            body.style.backgroundImage = "url('./img/background_dark.png')";
-            body.style.backgroundColor = '#551d1d';
-            body.style.backgroundPosition = 'top right';
-            body.style.height = '2000px';
-            mainWrapper.style.display = 'none';
-        }
-    }, 2000 );
-}
+  // Sets 2s timeout to delay background change
+  window.setTimeout(() => {
+    // Change styling for different screen sizes
+    if (html.clientWidth < 900) {
+      repairButton.style.display = "block";
+      repairButton.style.animation = "fadeIn 2.5s";
+      body.style.backgroundImage = "url('./img/background_dark.png')";
+      body.style.backgroundColor = "#551d1d";
+      body.style.backgroundSize = "contain";
+      body.style.backgroundPosition = "center 35%";
+      mainWrapper.style.display = "none";
+    } else if (html.clientWidth < 1300) {
+      repairButton.style.display = "block";
+      repairButton.style.animation = "fadeIn 2.5s";
+      body.style.backgroundImage = "url('./img/background_dark.png')";
+      body.style.backgroundColor = "#551d1d";
+      body.style.backgroundSize = "contain";
+      body.style.backgroundPosition = "center 80%";
+      body.style.height = "1300px";
+      mainWrapper.style.display = "none";
+    } else {
+      repairButton.style.display = "block";
+      repairButton.style.animation = "fadeIn 2.5s";
+      body.style.backgroundImage = "url('./img/background_dark.png')";
+      body.style.backgroundColor = "#551d1d";
+      body.style.backgroundSize = "contain";
+      body.style.height = "2200px";
+      mainWrapper.style.display = "none";
+    }
+  }, 2000);
+};
 
+// This function repairs the website and sets it back to normal
 const newRepair = () => {
-    body.style.backgroundColor = '#00bff3';
-    body.style.backgroundImage = "url('./img/background_light.png')";
-    mainWrapper.style.animation = 'fadeIn 2.5s';
-    mainWrapper.style.display = 'block';
-    body.style.height = 'initial';
-    repairButton.style.display = 'none';
-    destructButton.style.display = 'block';
-    destructButton.style.animation = 'fadeIn 2.5s';
-}
+  body.style.backgroundColor = "#00bff3";
+  body.style.backgroundImage = "url('./img/background_light.png')";
+  mainWrapper.style.animation = "fadeIn 2.5s";
+  mainWrapper.style.display = "block";
+  body.style.height = "100%";
+  body.style.backgroundSize = "cover";
+  repairButton.style.display = "none";
+  destructButton.style.display = "block";
+  destructButton.style.animation = "fadeIn 2.5s";
+};
 
-/* This event handler initializes the websites destruction */
-destructButton.addEventListener('click', () => {
-    /* Call newDestroy function */
-    newDestroy();
+/****
+  --- Build event listeners ---
+****/
+
+// This event listener initializes the websites destruction
+destructButton.addEventListener("click", () => {
+  // Call newDestroy function
+  newDestroy();
 });
 
-repairButton.addEventListener('click', () => {
-    newRepair();
+// This event listener in itializes the websites repair
+repairButton.addEventListener("click", () => {
+  // Call newRepair function
+  newRepair();
 });
--- js/menu.js
+++ js/menu.js
@@ -1,70 +1,90 @@
-const html = document.querySelector('html');
-const siteLogo = document.querySelector('.logoLi');
-const toggleMenu = document.querySelector('#toggle');
-const toggleItems = document.querySelector('#toggleItems');
-toggleMenu.className = 'closed';
+/*
+    This JavaScript file adds additional behavior to the navigation menu for mobile devices
+    
+    Created by: Ben Youngblood on 12/10/2018
+*/
+
+/**** 
+  --- Declaring variables ---
+****/
+
+const html = document.querySelector("html");
+const siteLogo = document.querySelector(".logoLi");
+const toggleMenu = document.querySelector("#toggle");
+const toggleItems = document.querySelector("#toggleItems");
+toggleMenu.className = "closed";
 toggleMenu.innerHTML = `<img src='img/toggleOff.png' alt='Menu toggled off'>`;
 
+/**** 
+    --- Build functions ---
+****/
+
 const toggleOn = () => {
-    toggleMenu.innerHTML = `<img src='img/toggleOn.png' alt='Menu toggled off'>`;
-    toggleItems.style.display = 'flex';
-    toggleItems.style.animation = 'slideFromRight 2s';
-    siteLogo.style.animation = 'fadeOut 2s';
-    siteLogo.style.display = 'none';
-    toggleMenu.style.animation = 'slideFromRight 2s';
-    toggleMenu.className.replace(/\bclosed\b/, '');
-    toggleMenu.className = 'open';
-}
+  toggleMenu.innerHTML = `<img src='img/toggleOn.png' alt='Menu toggled off'>`;
+  toggleItems.style.display = "flex";
+  toggleItems.style.animation = "slideFromRight 2s";
+  siteLogo.style.animation = "fadeOut 2s";
+  siteLogo.style.display = "none";
+  toggleMenu.style.animation = "slideFromRight 2s";
+  toggleMenu.className.replace(/\bclosed\b/, "");
+  toggleMenu.className = "open";
+};
 const toggleOff = () => {
-    toggleMenu.innerHTML = `<img src='img/toggleOff.png' alt='Menu toggled off'>`;
-    toggleItems.style.display = 'none';
-    toggleItems.style.animation = 'slideFromLeft 2s';
-    siteLogo.style.animation = 'fadeIn2 2s';
-    siteLogo.style.display = 'inline';
-    toggleMenu.style.animation = 'slideFromLeft 2s';
-    toggleMenu.className.replace(/\bopen\b/, '');
-    toggleMenu.className = 'closed';
-}
+  toggleMenu.innerHTML = `<img src='img/toggleOff.png' alt='Menu toggled off'>`;
+  toggleItems.style.display = "none";
+  toggleItems.style.animation = "slideFromLeft 2s";
+  siteLogo.style.animation = "fadeIn2 2s";
+  siteLogo.style.display = "inline";
+  toggleMenu.style.animation = "slideFromLeft 2s";
+  toggleMenu.className.replace(/\bopen\b/, "");
+  toggleMenu.className = "closed";
+};
 
-toggleMenu.addEventListener('click', () => {
-    if (toggleMenu.className.includes('closed')) {
-        toggleOn();
-    } else {
-        toggleOff();
-    }
+/****
+    --- Build event listeners ---
+****/
+
+// Calls toggleOn and toggleOff functions when clicking on menu toggle icons
+toggleMenu.addEventListener("click", () => {
+  if (toggleMenu.className.includes("closed")) {
+    toggleOn();
+  } else {
+    toggleOff();
+  }
 });
 
-//Layout that loads based on screen size on first load
-window.addEventListener('load', () => {
-    if (html.clientWidth > 750) {
-        toggleOn();
-        siteLogo.style.display = 'inline';
-        siteLogo.style.animation = 'none';
-        siteLogo.style.marginLeft = '15px';
-        toggleItems.style.display = 'flex';
-        toggleMenu.style.display = 'none';
-    } else {
-        toggleOff()
-        toggleItems.style.display = 'none';
-        toggleMenu.style.display = 'block';
-        toggleMenu.style.position = 'absolute';
-        toggleMenu.style.right = '15px';
-    }
+// Load these layout styles based on screen size on first load
+window.addEventListener("load", () => {
+  if (html.clientWidth > 750) {
+    toggleOn();
+    siteLogo.style.display = "inline";
+    siteLogo.style.animation = "none";
+    siteLogo.style.marginLeft = "15px";
+    toggleItems.style.display = "flex";
+    toggleMenu.style.display = "none";
+  } else {
+    toggleOff();
+    toggleItems.style.display = "none";
+    toggleMenu.style.display = "block";
+    toggleMenu.style.position = "absolute";
+    toggleMenu.style.right = "15px";
+  }
 });
-//Layout that loads based on screen size on resize
-window.addEventListener('resize', () => {
-    if (html.clientWidth > 750) {
-        toggleOn();
-        siteLogo.style.display = 'inline';
-        siteLogo.style.animation = 'fadeIn 1s';
-        siteLogo.style.marginLeft = '15px';
-        toggleItems.style.display = 'flex';
-        toggleMenu.style.display = 'none';
-    } else {
-        toggleOff();
-        toggleItems.style.display = 'none';
-        toggleMenu.style.display = 'inline';
-        toggleMenu.style.position = 'absolute';
-        toggleMenu.style.right = '15px';
-    }
+
+// Load these layout styles based on screen size on resize
+window.addEventListener("resize", () => {
+  if (html.clientWidth > 750) {
+    toggleOn();
+    siteLogo.style.display = "inline";
+    siteLogo.style.animation = "fadeIn 1s";
+    siteLogo.style.marginLeft = "15px";
+    toggleItems.style.display = "flex";
+    toggleMenu.style.display = "none";
+  } else {
+    toggleOff();
+    toggleItems.style.display = "none";
+    toggleMenu.style.display = "inline";
+    toggleMenu.style.position = "absolute";
+    toggleMenu.style.right = "15px";
+  }
 });
