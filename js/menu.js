b12a48bc1992be1b84862fe841740e38501fcfb2
Benstrocity <byoungblood.c@gmail.com>
2019-01-05 06:30:38 -0600

    update to navigation menu transition speed and another fix attempt for images showing on heroku for the portfolio page

--- js/menu.js
+++ js/menu.js
@@ -22,20 +22,20 @@ toggleMenu.innerHTML = `<img src='img/toggleOff.png' alt='Menu toggled off'>`;
 const toggleOn = () => {
   toggleMenu.innerHTML = `<img src='img/toggleOn.png' alt='Menu toggled off'>`;
   toggleItems.style.display = "flex";
-  toggleItems.style.animation = "slideFromRight 2s";
+  toggleItems.style.animation = "slideFromRight 1s";
   siteLogo.style.animation = "fadeOut 2s";
   siteLogo.style.display = "none";
-  toggleMenu.style.animation = "slideFromRight 2s";
+  toggleMenu.style.animation = "slideFromRight 1s";
   toggleMenu.className.replace(/\bclosed\b/, "");
   toggleMenu.className = "open";
 };
 const toggleOff = () => {
   toggleMenu.innerHTML = `<img src='img/toggleOff.png' alt='Menu toggled off'>`;
   toggleItems.style.display = "none";
-  toggleItems.style.animation = "slideFromLeft 2s";
-  siteLogo.style.animation = "fadeIn2 2s";
+  toggleItems.style.animation = "slideFromLeft 1s";
+  siteLogo.style.animation = "fadeIn2 1s";
   siteLogo.style.display = "inline";
-  toggleMenu.style.animation = "slideFromLeft 2s";
+  toggleMenu.style.animation = "slideFromLeft 1s";
   toggleMenu.className.replace(/\bopen\b/, "");
   toggleMenu.className = "closed";
 };
